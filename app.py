0fa307e4524e4312ed6fd7bec9d13838eb31abfb
Artgit01 <santoshpatil.art@gmail.com>
2026-02-02 20:04:37 +0530

    Initialize Flask app with home and ping routes

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import time
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Option Chain Fetcher Running"
+
+@app.route('/ping')
+def ping():
+    return "OK"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
